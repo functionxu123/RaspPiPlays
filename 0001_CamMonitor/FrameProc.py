e28b37a46f3abcc59387febc8bd4c6b36ddf4a6c
functionxu123 <1229853312@qq.com>
2022-02-11 23:29:18 +0800

    add fxi

--- 0001_CamMonitor/FrameProc.py
+++ 0001_CamMonitor/FrameProc.py
@@ -209,7 +209,8 @@ class CutThread(threading.Thread):
                         vid = None
                         print("Video %s finished" % vname)
                         map4vname=op.splitext(vname)[0]+".mp4"
-                        cmstr="ffmpeg -i "+vname+" -vcodec h264 "+ map4vname + " && rm -f " + vname +" >/dev/null 2>&1 &"
+                        cmstr="ffmpeg -i "+vname+" -vcodec h264 "+ map4vname + " >/dev/null 2>&1 " if not args.debug else " " +\
+                            " && rm -f "+ " >/dev/null 2>&1 " if not args.debug else " -v " + vname +" &"
                         #rett=os.popen(cmstr)
                         rett=os.system(cmstr)
                         if args.debug: 
